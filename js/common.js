ec6f3483f29eb6c2aa6b60a7f1d8f810d714ce84
Ruslan Voronenko <105813644+RusProgger@users.noreply.github.com>
2024-05-31 13:08:06 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	// Мобильное меню 
+	$(".fa-bars").click(function(e){
+		e.preventDefault();
+		$(".nav").slideToggle(500);
+	});
+	// Конец Мобильное меню
+
+	
+
+
+
+});//END READY
---
+++ js/fonts.js
@@ -0,0 +1,6 @@
+
+ 		$(document).ready(function() {
+ 			$("head").append('<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">');
+ 			$("head").append("<link href='//netdna.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.css' rel='stylesheet'>");
+ 		}); 
+ 		
